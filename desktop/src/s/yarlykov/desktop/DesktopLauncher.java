e27700161633bd0c2aed7135d5f25a4dfe4a187c
yagray <yagray@mail.ru>
2019-03-05 18:04:15 +0300

    desktop folder

---
+++ desktop/src/s/yarlykov/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package s.yarlykov.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import s.yarlykov.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
