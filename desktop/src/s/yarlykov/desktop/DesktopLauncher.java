c5133b8d105033ca105a6a8815f3e71941b84235
ciscoff <ciscoff@ya.ru>
2019-03-10 16:01:19 +0300

    change launch class

--- desktop/src/s/yarlykov/desktop/DesktopLauncher.java
+++ desktop/src/s/yarlykov/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package s.yarlykov.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import s.yarlykov.Star2DGame;
 import s.yarlykov.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
+		new LwjglApplication(new Star2DGame(), config);
 	}
 }
